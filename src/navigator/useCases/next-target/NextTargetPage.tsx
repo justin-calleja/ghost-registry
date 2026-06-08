5cadffc8c0063c28bba5551b0246feac7ea27013
Justin <justin.calleja@rocketfin.co>
2026-06-08 10:23:33 +0100

    add VITE_API_BASE_URL

--- src/navigator/useCases/next-target/NextTargetPage.tsx
+++ src/navigator/useCases/next-target/NextTargetPage.tsx
@@ -1,7 +1,14 @@
 import { Layout, Logo, Nav, NavItem, Stripe } from '@design-system';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export const NextTargetPage = () => {
+    useEffect(() => {
+        console.log('API_BASE_URL>>', API_BASE_URL);
+    }, []);
+
     return (
         <Layout>
             <Stripe>
